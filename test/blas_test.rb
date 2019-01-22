9d7b3e32b156a523a8dbb00c5f85cd7eab8dd633
wongalvis14 <wongalvis14@gmail.com>
2019-01-22 15:38:54 +0000

    Add blas tests for different dtypes

--- test/blas_test.rb
+++ test/blas_test.rb
@@ -1,21 +1,28 @@
-require 'test_helper'
+require_relative 'test_helper'
 
 class NMatrix::BlasTest < Minitest::Test
 
   def setup
     @left = NMatrix.new  [2,2],[2.2, 2.2, 4, 5]
     @right = NMatrix.new [2,2],[2, 2, 2, 2]
+    @dtypes = [:nm_float32, :nm_float64]
   end
 
   def test_dot
-    result = NMatrix.new  [2,2], [8.8, 8.8, 18.0, 18.0]
-    answer = @left.dot @right
-    assert_equal answer, result
+    @dtypes.each do |dtype|
+      left = NMatrix.new  [2,2],[2.2, 2.2, 4, 5], dtype
+      right = NMatrix.new [2,2],[2, 2, 2, 2], dtype
+      result = NMatrix.new  [2,2], [8.8, 8.8, 18.0, 18.0], dtype
+      answer = left.dot right
+      assert_equal answer, result
+    end
   end
 
   def test_norm
-    vector = NMatrix.new  [2,1], [3, 4]
-    assert_equal vector.norm, 5
+    @dtypes.each do |dtype|
+      vector = NMatrix.new  [2,1], [3.3, 4.8], dtype
+      assert_in_delta vector.norm, 5.8249, 1e-3
+    end
   end
 
 end
