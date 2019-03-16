4b1a2de0863992261dff8dd2c8e28d0490d171a2
uditgulati <uditgulati0@gmail.com>
2019-03-17 02:37:42 +0530

    Add nm_bool tests

--- test/nmatrix_test.rb
+++ test/nmatrix_test.rb
@@ -4,14 +4,17 @@ class NMatrix::CreationTest < Minitest::Test
 
   def setup
     @i = NMatrix.new [2,2],[1, 4.2, 3,4]
+    @b = NMatrix.new [2,2],[true, true, false, true], :nm_bool
   end
 
   def test_dims
     assert_equal [2,2], @i.shape
+    assert_equal [2,2], @b.shape
   end
 
   def test_elements
     assert_equal [1, 4.2, 3, 4], @i.elements
+    assert_equal [true, true, false, true], @b.elements
   end
 
   def test_accessor_get
