1d9c23e9c49e6c614e969bb16b73067388c8c6ed
Prasun Anand <prasunanand.bitsp@gmail.com>
2019-04-10 15:33:59 +0530

    concat

--- lib/nmatrix/numruby.rb
+++ lib/nmatrix/numruby.rb
@@ -4,10 +4,78 @@ module NumRuby
 
     if(options[:dtype])
       dtype = options[:dtype]
+    else
+      dtype = :nm_float64
     end
 
     NMatrix.new(shape, elements, dtype)
   end
 
+  def self.arange(len)
+    elements = Array(0..len)
+    NMatrix.new([len,1], elements, :nm_int)
+  end
+
+  # unary operations
+  def self.sin(obj)
+    if obj.is_a?(NMatrix)
+      obj.sin()
+    else
+      Math.sin(obj)
+    end
+  end
+
+  def self.append(a, b)
+    self.vstack([a,b])
+  end
+
+  def self.vstack(objs)
+    return nil unless objs.is_a?(Array)
+    rows = objs[0].shape[0] * objs.length
+    cols = objs[0].shape[1]
+    result = NumRuby.zeros([rows, cols])
+    objs.each do |obj|
+      #check num_cols mismatch
+      # (0..objs.shape[0]).each
+    end
+
+    # (0..result.shape[0]).each do |row_index|
+    #   result[row_index, *] = objs[x][row_index, *]
+    #   index = (row_index == rows ? 0 : index + 1)
+    # end
+    result
+  end
+
+  def self.hstack(objs)
+    return nil unless objs.is_a?(Array)
+    rows = objs[0].shape[0]
+    cols = objs[0].shape[1] * objs.length
+    result = NumRuby.zeros([rows, cols])
+    objs.each do |obj|
+      #check num_rows mismatch
+    end
+
+    # (0..result.shape[1]).each do |col_index|
+    #   result[col_index, *] = objs[x][col_index, *]
+    #   index = (col_index == cols ? 0 : index + 1)
+    # end
+    result
+  end
+
+  def self.dot(lha, rha)
+    lha.dot(rha)
+  end
+
+  module Linalg
+    def self.inv(obj)
+      if obj.is_a?(NMatrix)
+        return obj.invert
+      end
+    end
+
+    def self.solve
+
+    end
+  end
 
 end
--- test/numruby_test.rb
+++ test/numruby_test.rb
@@ -18,4 +18,30 @@ class NumRuby::CreationTest < Minitest::Test
     assert_equal [1, 4.2, 3, 4], @i.elements
   end
 
+  def test_arange
+    x = NumRuby.arange 5
+    assert_equal x.elements, [0,1,2,3,4]
+    assert_equal x.shape, [5, 1]
+    assert_equal x.dtype, :nm_int
+  end
+
+  def test_sin
+    x = NumRuby.sin(5)
+    assert_equal x, Math.send(:sin, 5)
+
+    x = NumRuby.sin(@i)
+    assert_equal x, @i.sin
+  end
+
+  def test_inv
+    assert_equal NumRuby::Linalg.inv(@i), @i.invert
+  end
+
+  def test_append
+    x = NumRuby.array [2,2],[0, 0, 0, 0]
+    y = NumRuby.array [2,2],[0, 0, 0, 0]
+    result = NumRuby.append(x, y)
+    assert_equal result, NumRuby.array([4,2],
+                                        [0, 0, 0, 0, 0, 0, 0, 0])
+  end
 end
