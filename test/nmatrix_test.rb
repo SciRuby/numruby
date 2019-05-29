66ca14d9a9c8b79d4171e6881fbfcc4f4c7f409d
uditgulati <uditgulati0@gmail.com>
2019-05-29 23:47:40 +0530

    Add more tests for n-dimensional matrices

--- test/nmatrix_test.rb
+++ test/nmatrix_test.rb
@@ -3,14 +3,17 @@ require 'test_helper'
 class NMatrix::CreationTest < Minitest::Test
 
   def setup
-    @i = NMatrix.new [2,2],[1, 4.2, 3,4]
+    @i = NMatrix.new [2,2],[1, 4.2, 3, 4]
     @b = NMatrix.new [2,2],[true, true, false, true], :nm_bool
-    @m = NMatrix.new [2,2,2],[1,2,3,4,5,6,7,8]
+    @m = NMatrix.new [2,2,2],[1, 2, 3, 4, 5, 6, 7, 8]
+    @n = NMatrix.new [2,1,2],[1, 2, 3, 4]
   end
 
   def test_dims
     assert_equal [2,2], @i.shape
     assert_equal [2,2], @b.shape
+    assert_equal [2,2,2], @m.shape
+    assert_equal [2,1,2], @n.shape
   end
 
   def test_dtype
@@ -24,11 +27,15 @@ class NMatrix::CreationTest < Minitest::Test
   def test_elements
     assert_equal [1, 4.2, 3, 4], @i.elements
     assert_equal [true, true, false, true], @b.elements
+    assert_equal [1, 2, 3, 4, 5, 6, 7, 8], @m.elements
+    assert_equal [1, 2, 3, 4], @n.elements
   end
 
   def test_accessor_get
     assert_equal @i[0,1], 4.2
     assert_equal @m[0,0,1], 2
+    assert_equal @n[1,0,0], 3
+    assert_equal @n[1,0,1], 4
   end
 
   def test_accessor_set
@@ -36,6 +43,8 @@ class NMatrix::CreationTest < Minitest::Test
     assert_equal @i[0,0], 6
     @m[1,1,0] = 11
     assert_equal @m[1,1,0], 11
+    @n[0,0,1] = 12
+    assert_equal @n[0,0,1], 12
   end
 
 end
