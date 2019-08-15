03cf1635d752fc844c9b43a4d5beea03c76c2070
uditgulati <uditgulati0@gmail.com>
2019-08-16 02:07:27 +0530

    Wriet tests for geqrf, orgqr and potrf

--- test/lapack_test.rb
+++ test/lapack_test.rb
@@ -4,6 +4,9 @@ class NMatrix::LapackTest < Minitest::Test
 
   def setup
     @input = NMatrix.new [2,2], [2, -1, -4, 3]
+    @matrix1 = NMatrix.new [2, 3], [4, -1, 4, 0, 7, -5]
+    @matrix2 = NMatrix.new [3, 2], [4, -1, 4, 0, 7, -5]
+    @matrix3 = NMatrix.new [3, 3], [1, 0, -5, 3, 5, 2, 0, -1, -1]
     @left = NMatrix.new  [2,2],[2.2, 2.2, 4, 5]
     @right = NMatrix.new [2,2],[2, 2, 2, 2]
     @dtypes = [:nm_float64]
@@ -41,6 +44,86 @@ class NMatrix::LapackTest < Minitest::Test
       solution = NMatrix.new [2,1], [9.5, 15], dtype
       assert_equal input.least_square(rhs), solution
     end
+  end
+
+  def test_geqrf
+    qr, tau = NumRuby::Lapack.geqrf(@matrix1)
+    qr_soln = NMatrix.new [2, 3], [4.0, -1.0, 4.0, 0.0, 7.0, -5.0]
+    tau_soln = NMatrix.new [2], [0.0, 0.0]
+    assert_equal qr, qr_soln
+    assert_equal tau, tau_soln
+
+    qr, tau = NumRuby::Lapack.geqrf(@matrix2)
+    qr_soln = NMatrix.new [3, 2], [-9.0, 4.333333, 0.307692, -2.687419, 0.538461, -0.491678]
+    tau_soln = NMatrix.new [2], [1.444444, 1.610632]
+    assert_equal qr, qr_soln
+    assert_equal tau, tau_soln
+    
+    qr, tau = NumRuby::Lapack.geqrf(@matrix3)
+    qr_soln = NMatrix.new [3, 3], [-3.162277, -4.743416, -0.316227, 0.720759, -1.870828, -5.077963, 0.0, -0.289689, 2.028370]
+    tau_soln = NMatrix.new [3], [1.316227, 1.845154, 0.0]
+    assert_equal qr, qr_soln
+    assert_equal tau, tau_soln
+  end
+
+  def test_orgqr
+    qr = NMatrix.new [3, 2], [-9.0, 4.333333, 0.307692, -2.687419, 0.538461, -0.491678]
+    tau = NMatrix.new [2], [1.444444, 1.610632]
+    q = NumRuby::Lapack.orgqr(qr, tau)
+    q_soln = NMatrix.new [3, 2], [-0.444444, -0.344540, -0.444444, -0.716645, -0.777777, 0.606392]
+    assert_equal q, q_soln
+  end
+
+  def test_geqp3
+    ## doesn't give same output on each run
+  end
+
+  def test_potrf
+    matrix = NMatrix.new [3, 3], [2, -1, 0, -1, 2, -1, 0, -1, 2]
+
+    c = NumRuby::Lapack.potrf(matrix, true)
+    c_soln = NMatrix.new [3, 3], [1.414213, -1.0, 0.0, -0.707106, 1.224744, -1.0, 0.0, -0.816496, 1.154700]
+    assert_equal c, c_soln
+
+    c = NumRuby::Lapack.potrf(matrix, false)
+    c_soln = NMatrix.new [3, 3], [1.414213, -0.707106, 0.0, -1.0, 1.224744, -0.816496, 0.0, -1.0, 1.154700]
+    assert_equal c, c_soln
+  end
+
+  def test_potrs
+
+  end
+
+  def test_gesdd
+
+  end
+
+  def test_getrf
+
+  end
+
+  def test_getrs
+
+  end
+
+  def test_getri
+
+  end
+
+  def test_gelss
+
+  end
+
+  def test_posv
+
+  end
+
+  def test_gesv
+
+  end
+
+  def test_lange
+
   end
 
   def test_pinv
