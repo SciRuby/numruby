0e3275eaec94662a3c79e9105da285f427301e17
uditgulati <uditgulati0@gmail.com>
2019-09-13 03:34:36 +0530

    pretty print working for dim = 2

--- lib/nmatrix/nmatrix.rb
+++ lib/nmatrix/nmatrix.rb
@@ -8,9 +8,46 @@ class NMatrix
   # @example
   #  TODO
   def pretty_print(q)
-    if self.shape.size > 1 and self.shape[1] > 100
-      self.inspect.pretty_print(q)
+    if self.dim == 1
+      
+    elsif self.dim == 2
+      # iterate through the whole matrix and find the longest number for each column
+      longest = Array.new(self.shape[1], 0)
+
+      for col_index in 0...self.shape[1] do
+        j = col_index
+        self.shape[0].times do
+          elem_len           = self.elements[j].inspect.size
+          longest[col_index] = elem_len if longest[col_index] < elem_len
+          j += self.shape[1]
+        end
+      end
+
+      q.group(0, "\n[\n ", "]") do
+        for row_index in 0...self.shape[0] do
+          i = (row_index * self.shape[1])
+          current_row = Array.new
+          self.shape[1].times do
+            current_row.push(elements[i])
+            i += 1
+          end
+          q.group(1, " [", "]\n") do
+            q.seplist(current_row, -> { q.text ", " }, :each_with_index) do |v,j|
+              q.text v.inspect.rjust(longest[j])
+            end
+          end
+          q.breakable unless row_index + 1 == self.shape[0]
+        end
+      end
+    elsif self.dim == 3
+      
+    else
+
     end
+  end
+
+  def to_a
+    return self.elements
   end
 
   def inspect #:nodoc:
